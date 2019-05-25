4df4205becd9cb7c1b4c1185a50f3a1c03682cd0
Zachary Hyde <zachary.a.hyde@gmail.com>
2019-05-25 15:13:38 -0400

    initial commit

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+//hello.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello!")
+	fmt.Println("Hello! 1")
+	fmt.Println("Hello! 2")
+	fmt.Println("Hello! 3")
+	fmt.Println("Hello! 4")
+}
